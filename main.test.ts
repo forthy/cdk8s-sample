496e3f7baece94dfae155993f644a3ed1e792473
Richard Chuo <richard.chuo@gmail.com>
2022-02-23 12:54:05 +0800

    Initial commit

---
+++ main.test.ts
@@ -0,0 +1,11 @@
+import { SampleChart } from './main'
+import { Testing } from 'cdk8s'
+
+describe('Placeholder', () => {
+    test('Empty', () => {
+        const app = Testing.app()
+        const chart = new SampleChart(app, 'test-chart')
+        const results = Testing.synth(chart)
+        expect(results).toMatchSnapshot()
+    })
+})
